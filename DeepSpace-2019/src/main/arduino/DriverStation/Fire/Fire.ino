134e559885b03009643aa6db0a9b85b782c4ae92
wh1ter0se <rckreischer@gmail.com>
2019-03-21 09:37:37 -0500

    Worked on arduino patterns a bit.

--- DeepSpace-2019/src/main/arduino/DriverStation/Fire/Fire.ino
+++ DeepSpace-2019/src/main/arduino/DriverStation/Fire/Fire.ino
@@ -10,9 +10,14 @@
 // On a Trinket or Gemma we suggest changing this to 1
 #define PIN            6
 
+#define THREE          26
+#define SIX            32
+#define NINE           38
+#define FIVE           44
+#define LOOP           50
+
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      120
-
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
@@ -23,47 +28,48 @@ void setup() {
 }
 
 void loop() {
-
+//rainbowCycle(0);
 //  opposingColors();
-  getCapitalismed();
+//  getCapitalismed();
+redFire();
 //  blueFire();
-//    delay(random(50,100)); // Delay for a period of time (in milliseconds). 
-  
+    
+//  redNumbers();
+//  capitalistNumbers();
 }
 
 void redFire() {
   for(int i=0;i< NUMPIXELS;i++){
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     int flicker= random(1, 15);
-    int r = 255 - flicker * 10;
+    int r = 255 - flicker * 16;
     int g = flicker;
     int b = 0;
-    int w = flicker;
+    int w = flicker / 2;
     pixels.setPixelColor(i, pixels.Color(g, r, b, w)); 
   }
     pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(random(50,100)); // Delay for a period of time (in milliseconds). 
 }
 
 void blueFire() {
   for(int i=0;i< NUMPIXELS;i++){
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     int flicker= random(1, 15);
-    int r = 0;
+    int r = flicker;
     int g = r / 32;
-    int b = 255 - flicker * 16;
+    int b = 255 - flicker * 24;
     int w = flicker;
     pixels.setPixelColor(i, pixels.Color(g, r, b, w)); 
   }
     pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(random(50,100)); // Delay for a period of time (in milliseconds). 
 }
 
 void getCapitalismed() {
   for(int i=0;i<(NUMPIXELS/3);i++) {
     pixels.setPixelColor(i, pixels.Color(0,0,255,0));
   }
-//  for(int i=1;i<(NUMPIXELS/3);i+=2) {
-//    pixels.setPixelColor(i, pixels.Color(0,0,0,255));
-//  }
   for (int i=(NUMPIXELS/3);i<NUMPIXELS;i+=2) {
     pixels.setPixelColor(i, pixels.Color(0,255,0,0));
   }
@@ -138,4 +144,105 @@ void opposingColors2() {
     pixels.show();
     delay(10);
   }
+}
+
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + j) & 255));
+    }
+    pixels.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0, 255);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3, 255);
+  } else {
+   WheelPos -= 170;
+   return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3, 255);
+  }
+}
+
+void redFireOnLoop() {
+  for(int i=LOOP;i< NUMPIXELS;i++){
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    int flicker= random(1, 15);
+    int r = 255 - flicker * 10;
+    int g = flicker;
+    int b = 0;
+    int w = flicker;
+    pixels.setPixelColor(i, pixels.Color(g, r, b, w)); 
+  }
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    // delay(random(50,100)); // Delay for a period of time (in milliseconds). 
+}
+
+void redNumbers() {
+  int wait = 500;
+  for (int i=0;i<NUMPIXELS;i++) {
+    pixels.setPixelColor(i, pixels.Color(0,0,0,0));
+  }
+  pixels.show();
+  for (int i=THREE;i<SIX;i++) {
+    pixels.setPixelColor(i, pixels.Color(0,255,0,0));
+  }
+  pixels.show();
+  delay(wait);
+  pixels.show();
+  for (int i=SIX;i<NINE;i++) {
+    pixels.setPixelColor(i, pixels.Color(0,255,0,0));
+  }
+  pixels.show();
+  delay(wait);
+  pixels.show();
+  for (int i=NINE;i<FIVE;i++) {
+    pixels.setPixelColor(i, pixels.Color(0,255,0,0));
+  }
+  pixels.show();
+  delay(wait);
+  pixels.show();
+  for (int i=FIVE;i<LOOP;i++) {
+    pixels.setPixelColor(i, pixels.Color(0,255,0,0));
+  }
+  pixels.show();
+  delay(wait);
+}
+
+void capitalistNumbers() {
+  for(int i=THREE;i<NINE;i++) {
+    pixels.setPixelColor(i, pixels.Color(0,0,255,0));
+  }
+  for (int i=NINE;i<FIVE;i++) {
+    pixels.setPixelColor(i, pixels.Color(0,255,0,0));
+  }
+  for (int i=FIVE;i<LOOP;i++) {
+    pixels.setPixelColor(i, pixels.Color(0,0,0,255));
+  }
+  pixels.show();
+  delay(200);
+  for(int i=THREE;i<NINE;i++) {
+    pixels.setPixelColor(i, pixels.Color(0,0,255,0));
+  }
+//  for(int i=1;i<(NUMPIXELS/3);i+=2) {
+//    pixels.setPixelColor(i, pixels.Color(0,0,0,255));
+//  }
+  for (int i=NINE;i<FIVE;i++) {
+    pixels.setPixelColor(i, pixels.Color(0,0,0,255));
+  }
+  for (int i=FIVE;i<LOOP;i++) {
+    pixels.setPixelColor(i, pixels.Color(0,255,0,0));
+  }
+  pixels.show();
+  delay(200);
 }
